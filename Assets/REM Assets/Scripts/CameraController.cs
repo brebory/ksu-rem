c1805aee5260b931292611676640bc9fd9a58c6a
Brian Boll <bboll@kent.edu>
2014-02-12 20:41:11 -0500

    Fixed camera, removed it from character movement

---
+++ Assets/REM Assets/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+public class CameraController : MonoBehaviour 
+{
+	public GameObject target;
+	// Higher number provides greatly reduced "realistic" camera vibration
+	public float damping = 1;
+	Vector3 offset;
+	void Start() {
+		offset = transform.position - target.transform.position;
+	}
+	void Update() {
+	}
+	void LateUpdate() {
+		Vector3 desiredPosition = target.transform.position + offset;
+		Vector3 position = Vector3.Lerp(transform.position, desiredPosition, Time.deltaTime * damping);
+		transform.position = position;
+		transform.LookAt(target.transform.position);
+	}
+	// LateUpdate is called once per frame after player movement
+}
