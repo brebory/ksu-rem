6209cf7efec66ebeac467520a82cec198aa8377e
Brendon Roberto <brendon@brendonroberto.com>
2014-02-21 13:36:03 -0500

    Create InputBroadcaster to change keypresses to game events.

---
+++ Assets/REM Assets/Scripts/Controls/AttackControl.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AttackControl : MonoBehaviour {
+
+	private GameObject player;
+
+	// Use this for initialization
+	void Start () {
+
+		// Cache and store player object
+		player = (GameObject)GameObject.FindGameObjectWithTag("Player");
+		Messenger.AddListener("InputAttackEvent1", OnInputAttackEvent1);
+		Messenger.AddListener("InputAttackEvent2", OnInputAttackEvent2);
+		Messenger.AddListener("InputAttackEvent3", OnInputAttackEvent3);
+		Messenger.AddListener("InputAttackEvent4", OnInputAttackEvent4);
+		Messenger.AddListener("InputAttackComboEvent", OnInputAttackComboEvent);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnInputAttackEvent1() {
+
+	}
+
+	void OnInputAttackEvent2() {
+
+	}
+
+	void OnInputAttackEvent3() {
+
+	}
+
+	void OnInputAttackEvent4() {
+
+	}
+
+	void OnInputAttackComboEvent() {
+
+	}
+}
---
+++ Assets/REM Assets/Scripts/Controls/InputBroadcaster.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputBroadcaster : MonoBehaviour {
+
+	private bool paused;
+
+	// Use this for initialization
+	void Start() {
+		paused = false;
+		Messenger.AddListener("GamePause", OnGamePause);
+		Messenger.AddListener("GameResume", OnGameResume);
+	}
+	
+	// Update is called once per frame
+	void Update() {
+		if (!paused) {
+			if (Input.GetKey(KeyCode.Q)) {
+				Messenger.Broadcast("InputAttackEvent1");
+			}
+
+			if (Input.GetKey(KeyCode.W)) {
+				Messenger.Broadcast("InputAttackEvent2");
+			}
+
+			if (Input.GetKey(KeyCode.E)) {
+				Messenger.Broadcast("InputAttackEvent3");
+			}
+
+			if (Input.GetKey(KeyCode.R)) {
+				Messenger.Broadcast("InputAttackEvent4");
+			}
+
+			if (Input.GetKey(KeyCode.Space)) {
+				Messenger.Broadcast("InputAttackComboEvent");
+			}
+		}
+	}
+
+	void OnGamePause() {
+		paused = true;
+	}
+
+	void OnGameResume() {
+		paused = false;
+	}
+}
