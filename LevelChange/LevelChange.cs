674c94b5abc123cf963120bb61dd21c037d5e774
Kevin Whalen <kwhalen2@kent.edu>
2014-02-21 22:02:45 -0500

    initialized a scripts only branch

---
+++ LevelChange/LevelChange.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelChange : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	// Level Changing
+	void OnTriggerEnter(Collider other){
+		if (other.CompareTag("Player")){
+			switch (Application.loadedLevelName){
+				case "First":
+					Application.LoadLevel("Second");
+					break;
+				case "Second":
+					Application.LoadLevel("Third");
+					break;
+				case "Third":
+					Application.LoadLevel("Fourth");
+					break;
+				default:
+					Debug.Log("Error Changing Level");
+					break;
+			}
+		}
+	}
+}
